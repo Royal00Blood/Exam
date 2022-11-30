78520d5e12914f0a302de1de1e4902b31c47662e
Ivan <nazgoool1@gmail.com>
2022-11-30 23:04:03 +0400

    create metod -create new array

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿string [] array = new string [5]{"Vasy","add","Float","Cats","BOB"};
+
+string [] CreateNewArray (string [] array)
+{
+    string [] newArray = new string [array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length >= 3)
+        {
+            newArray[i] = array[i];
+        }
+    }
+    return newArray;
+}
